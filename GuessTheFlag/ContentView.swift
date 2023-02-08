7430a9ca5e2c0cf4f51740c01a476b455281f898
Thaddäus Schima <t.schima@mailbox.org>
2023-02-08 14:58:21 +0100

    added basic logic

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 1...2)
     var body: some View {
-        VStack {
-            
+        ZStack {
+            Color.gray.ignoresSafeArea()
+            VStack(spacing: 30){
+                VStack {
+                    Text("Tap the flag")
+                    Text(countries[correctAnswer])
+                    
+                }.foregroundColor(.white)
+                
+                ForEach(0..<3) {number in
+                    Button {
+                        //flag was tapped
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
         }
     }
 }
