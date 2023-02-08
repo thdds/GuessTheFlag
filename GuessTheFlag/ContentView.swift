8c888724bafd8e2040afc211af51dfc86a20e74b
Thaddäus Schima <t.schima@mailbox.org>
2023-02-08 15:07:22 +0100

    added the logic, when a flag gets tapped

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 1...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 1...2)
     var body: some View {
         ZStack {
             Color.gray.ignoresSafeArea()
@@ -22,15 +24,32 @@ struct ContentView: View {
                 
                 ForEach(0..<3) {number in
                     Button {
-                        //flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
                     }
                 }
             }
+        }.alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
         }
     }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "correct"
+        } else {
+            scoreTitle = "wrong"
+        }
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
